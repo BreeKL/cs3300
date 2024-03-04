75c3753e465fe979c7604fc4e96671e00de3d9f1
Brianne <leathbk1512@gmail.com>
2024-03-04 11:52:43 -0700

    Starting sprint03

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 # Create your views here.
 def index(request):
-
-# Render index.html
-    return render( request, 'portfolio_app/index.html')
+    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio_is_active=True)
+    print("active portfolio query set", student_active_portfolios)
+    # Render index.html
+    return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
 
