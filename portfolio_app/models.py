287f1c8f91a5e786319d7c0afbb105358da8f212
Brianne <leathbk1512@gmail.com>
2024-03-12 19:44:43 -0600

    Container for students with active portfolios working

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Portfolio(models.Model):
-    title = models.CharField(max_length=200, default="Title")
+    title = models.CharField(max_length=200)
     contact_email = models.CharField(max_length=200)
     is_active = models.BooleanField(default = False)
     about = models.TextField(blank=True)
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -4,7 +4,7 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio_is_active=True)
+    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
     print("active portfolio query set", student_active_portfolios)
     # Render index.html
     return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
