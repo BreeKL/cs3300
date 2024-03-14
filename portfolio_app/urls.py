badc2a9b7219beb4148958a42d4664ede414164e
Brianne <leathbk1512@gmail.com>
2024-03-14 12:05:19 -0600

    Students tab working, student list working, student detail working, portfolio detail working, and project detail working

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+#from portfolio_app.views import StudentListView, StudentDetailView
 
 urlpatterns = [
 #path function defines a url pattern
@@ -8,4 +9,10 @@ urlpatterns = [
 # name='index' parameter is to dynamically create url
 # example in html <a href="{% url 'index' %}">Home</a>.
 path('', views.index, name='index'),
+path('login/', views.login, name='login'),
+path('logout/', views.logout, name='logout'),
+path('students/', views.StudentListView.as_view(), name='students'),
+path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
+path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail'),
+path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,37 @@ def index(request):
     # Render index.html
     return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
 
+def login(request):
+    return render (HttpResponse, 'Login page')
+def logout(request):
+    return render (HttpResponse, 'Logout Page')
+
+# def student_list(request):
+#     student_list = Student.objects.select_related('name').all()
+#     print("active student_list query set:", student_list)
+#     return render( request, 'portfolio_app/student_list.html', {'student_list':student_list})
+
+# def student_detail(request):
+#     return render( request, 'portfolio_app/student_detail.html')
+
+# def portfolio_detail(request, id):
+#     project_list = Project.objects.all()
+#     print("Projects in portfolio: ", project_list)
+#     return render( request, 'portfolio_app/portfolio_detail.html', {'project_list':project_list})
+
+class StudentListView(ListView):
+    model = Student
+
+class StudentDetailView(DetailView):
+    model = Student
+
+class PortfolioDetailView(DetailView):
+    model = Portfolio
+    # project_list = Project.objects.all()
+
+class ProjectListView(ListView):
+    model = Project
+
+class ProjectDetailView(DetailView):
+    model = Project
+    
